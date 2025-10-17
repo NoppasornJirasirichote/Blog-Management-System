a6f9f86c0183b2dbd67faa134d54b0ea2d965386
NoppasornJirasirichote <noppasorn.jir.work.com>
2025-10-17 22:12:09 +0700

    first commit

--- backend/server.js
+++ backend/server.js
@@ -115,6 +115,40 @@
 // app.listen(PORT, () => console.log(`✅ Server running at http://localhost:${PORT}`));
 
 
+// require('dotenv').config();
+// const express = require('express');
+// const cors = require('cors');
+// const { createClient } = require('@supabase/supabase-js');
+
+// const app = express();
+// app.use(cors());
+// app.use(express.json());
+
+// // เชื่อม Supabase
+// const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
+
+// // API: GET users
+// app.get('/api/users', async (req, res) => {
+//   const { data, error } = await supabase.from('users').select('*');
+//   if (error) return res.status(400).json({ error: error.message });
+//   res.json(data);
+// });
+
+// // API: POST user
+// app.post('/api/users', async (req, res) => {
+//   const { email, name, password } = req.body;
+//   const { data, error } = await supabase
+//     .from('users')
+//     .insert([{ email, name, password }])
+//     .select();
+
+//   if (error) return res.status(400).json({ error: error.message });
+//   res.json(data);
+// });
+
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
+
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
@@ -127,15 +161,16 @@ app.use(express.json());
 // เชื่อม Supabase
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
 
-// API: GET users
-// app.get('/api/users', async (req, res) => {
-//   const { data, error } = await supabase.from('users').select('*');
-//   if (error) return res.status(400).json({ error: error.message });
-//   res.json(data);
-// });
-
+// Route test
 app.get('/', (req, res) => {
-  res.send('Server is running!');
+  res.json({ message: "Hello World!" });
+});
+
+// API: GET users
+app.get('/api/users', async (req, res) => {
+  const { data, error } = await supabase.from('users').select('*');
+  if (error) return res.status(400).json({ error: error.message });
+  res.json(data);
 });
 
 // API: POST user
@@ -151,4 +186,4 @@ app.post('/api/users', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`✅ Server running at http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
