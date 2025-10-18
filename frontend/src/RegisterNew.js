8561b1acf7ba88bedb48c16ed91490b502e0961a
NoppasornJirasirichote <noppasorn.jir.work.com>
2025-10-19 04:04:00 +0700

    2 commit

--- frontend/src/RegisterNew.js
+++ frontend/src/RegisterNew.js
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';+
+
 
 function Register() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [name, setname] = useState("");
     const [message, setMessage] = useState("");
-    
+    const API_URL = process.env.REACT_APP_API_URL;
+
+
 
 
     const navigate = useNavigate();
@@ -21,7 +24,7 @@ function Register() {
         e.preventDefault();
         console.log("กำลังส่งข้อมูลไป backend:", { email, name, password });
         try {
-            const res = await axios.post(`https://blog-management-system-fornt.onrender.com/Register`, { email, name, password });
+            const res = await axios.post(`${API_URL}/Register`, { email, name, password });
             console.log("Response จาก backend:", res.data);
             navigate("/Home", { state: { email: email } });
             setMessage(res.data.message);
