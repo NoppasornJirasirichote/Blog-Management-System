ea8d5c63abfd4413f58133b6e535ddacc182413b
NoppasornJirasirichote <noppasorn.jir.work.com>
2025-10-19 11:47:50 +0700

    21 commit

--- frontend/src/LoginPage.js
+++ frontend/src/LoginPage.js
@@ -44,7 +44,7 @@ function LoginPage() {
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
                     required
-                    style={{ width: "300px", height: "30px", marginBottom: "10px", padding: "5px" }}
+                    style={{ width: "100%", height: "30px", marginBottom: "10px", padding: "5px" }}
                 />
                 <input
                     type="password"
@@ -52,7 +52,7 @@ function LoginPage() {
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
                     required
-                    style={{ width: "300px", height: "30px", marginBottom: "10px", padding: "5px" }}
+                    style={{ width: "100%", height: "30px", marginBottom: "10px", padding: "5px" }}
                 />
                 <button type="submit" className={styles.butsubmit}>Login</button>
                 <button onClick={goToRegister} className={styles.butregister} >register</button>
